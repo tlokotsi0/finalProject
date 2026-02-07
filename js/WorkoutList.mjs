02cae485769b3de93af3e9dfb3ba064dae7e744b
tlokotsi0 <amantusf@gmail.com>
2026-02-07 13:40:21 +0200

    Second commit

--- js/WorkoutList.mjs
+++ js/WorkoutList.mjs
@@ -1,26 +1,47 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function exerciseTemplate(exercise) {
+    const apiKey = 'c68ad3903dmsh3834323f3cbd19cp12047bjsnde4c82474c93'; 
+    const imageUrl = `https://exercisedb.p.rapidapi.com/image/${exercise.id}?rapidapi-key=${apiKey}`;
+
     return `<li class="exercise-card">
-        <img src="${exercise.gifUrl}" alt="${exercise.name}">
-        <h3>${exercise.name}</h3>
-        <p>Target: ${exercise.target}</p>
+        <img src="${imageUrl}" alt="${exercise.name}" loading="lazy" 
+             onerror="this.src='https://placehold.co/300x300?text=Image+Not+Found'">
+        <h3 class="exercise-name">${exercise.name}</h3>
+        <p class="exercise-target">Target: ${exercise.target}</p>
+        <button class="add-to-routine">Add to Routine</button>
     </li>`;
 }
 
 export default class WorkoutList {
-    constructor(muscle, dataSource, listElement) {
-        this.muscle = muscle;
+    constructor(dataSource, listElement) {
         this.dataSource = dataSource;
         this.listElement = listElement;
     }
 
-    async init() {
-        const list = await this.dataSource.getExerciseData(this.muscle);
-        this.renderList(list);
+    async init(searchTerm, type = "target") {
+        this.listElement.innerHTML = "<li>Loading...</li>";
+        
+        try {
+            let list = [];
+            if (type === "target") {
+                list = await this.dataSource.getExerciseData(searchTerm);
+            } else if (type === "bodyPart") {
+                list = await this.dataSource.getExercisesByBodyPart(searchTerm);
+            }
+
+            if (list && list.length > 0) {
+                this.renderList(list);
+            } else {
+                this.listElement.innerHTML = "<li>No exercises found. Try 'waist', 'shoulders', or 'cardio'.</li>";
+            }
+        } catch (err) {
+            console.error(err);
+            this.listElement.innerHTML = "<li>Error loading data.</li>";
+        }
     }
 
     renderList(list) {
-        renderListWithTemplate(exerciseTemplate, this.listElement, list);
+        renderListWithTemplate(exerciseTemplate, this.listElement, list, true);
     }
 }
